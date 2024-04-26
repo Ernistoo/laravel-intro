3cad88fb3972a4c2b753e3156a07cfa66f7ba5b0
eihents <eihents@vtdt.edu.lvgit>
2024-04-26 09:28:43 +0300

    first commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,9 +7,22 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $posts = Post::all();
-        
+
         return view("posts.index", ["posts" => $posts]);
+    }
+
+    public function show($id)
+    {
+        $post = Post::find($id);
+
+        return view("posts.show", ["post" => $post]);
+    }
+
+    public function create()
+    {
+        return view("posts.create");
     }
 }
